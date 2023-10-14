cc361a216799877f0a729dfb1e19f036a7d5b89a
LiudmylaHorobets <horobets1323@gmail.com>
2023-10-14 22:15:57 +0300

    added statistics section

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => (
+  <div>
+    <p>Good: {good}</p>
+    <p>Neutral: {neutral}</p>
+    <p>Bad: {bad}</p>
+    <p>Total: {total}</p>
+    <p>Positive Feedback: {positivePercentage}%</p>
+  </div>
+);
